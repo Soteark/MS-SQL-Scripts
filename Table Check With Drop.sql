163915c614b335c09a0881ccf4fedae45b86be72
Soteark <slash.young@gmail.com>
2014-11-05 20:13:46 -0600

    added Table Drop query

---
+++ Table Check With Drop.sql
@@ -0,0 +1,7 @@
+/*Non Temp DB Table Check with Drop*/
+if exist ( select 1 from information_schema.tables where table_name = '[TABLE NAME]' )
+	drop table [TABLE NAME];
+
+/*Temp DB Table Check with Drop*/
+if object_id('tempdb..#[TABLE NAME]') is not null
+	drop table [TABLE NAME];
