9e11ea2ccffb35735677c17642a4fdba3a3ced94
Soteark <slash.young@gmail.com>
2014-11-05 21:02:40 -0600

    corrected syntax

--- Table Check With Drop.sql
+++ Table Check With Drop.sql
@@ -4,4 +4,4 @@ if exist ( select 1 from information_schema.tables where table_name = '[TABLE NA
 
 /*Temp DB Table Check with Drop*/
 if object_id('tempdb..#[TABLE NAME]') is not null
-	drop table [TABLE NAME];
+	drop table #[TABLE NAME];
