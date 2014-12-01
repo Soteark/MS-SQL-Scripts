70346a94052c464847ac85a094f1d6bfdddc08a7
Soteark <slash.young@gmail.com>
2014-12-01 14:09:11 -0600

    Updated DB Mail Script

--- DB Mail Check.sql
+++ DB Mail Check.sql
@@ -1,4 +1,19 @@
+--Check DB Mail Status==========================================
 select * from msdb.dbo.sysmail_sentitems
 select * from msdb.dbo.sysmail_unsentitems 
 select * from msdb.dbo.sysmail_faileditems
 order by mailitem_id desc
+
+--Send DB Mail==================================================
+exec msdb.dbo.sp_send_dbmail
+	@profile_name = 'SQLEmail_Account'
+	,@recipients = 'user@domain.com'
+	,@body = 'The Stored Procedure finished successfully'
+	,@subject = 'Automated Success Message'
+	,@query = '	select top 10
+					t1.Col1
+					,t1.Col2
+					,t1.Col3
+					,t1.Col4
+				from Database.dbo.Table t1'
+	,@attach_query_result_as_file = 1;
