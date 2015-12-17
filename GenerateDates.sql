715858022db94026f7c05e09bacb4dea59be04c3
Soteark <slash.young@gmail.com>
2015-12-16 22:30:08 -0600

    Create GenerateDates.sql

---
+++ GenerateDates.sql
@@ -0,0 +1,22 @@
+DECLARE @tNumTbl TABLE ( Number INT );
+
+WITH
+	CTE_Numbers ( Number ) AS (
+		SELECT 0 [Number]
+		UNION ALL
+		SELECT CN.Number + 1
+		FROM CTE_Numbers CN
+		WHERE CN.Number <= 3649)
+
+INSERT INTO @tNumTbl ( Number )
+SELECT CN.Number
+FROM CTE_Numbers CN
+OPTION ( MAXRECURSION 4000 );
+
+DECLARE
+	@dStartDt DATE = DATEADD(YEAR,-5,GETDATE())
+	,@dEndDt DATE = GETDATE()
+
+SELECT DATEADD(DAY,Number+1,@dStartDt) [Date]
+FROM @tNumTbl TNT
+WHERE DATEADD(DAY,Number+1,@dStartDt) <= @dEndDt
